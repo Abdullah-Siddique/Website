66f4ef47616106d4740d1df2d8801ff268f910c8
Abdullah Siddique <siddiqueabdullah581@gmail.com>
2024-03-06 12:03:07 +0600

    Replace repository contents with VSCode project

---
+++ frontend/build/precache-manifest.93305ebd10dffaf291dbf689034e789c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7eab6f4d41d7f24783e9bf03d2ea28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "197e6aad27bf5f5ab16d",
+    "url": "/static/css/main.2a8b6f19.chunk.css"
+  },
+  {
+    "revision": "beb3d9518878978f53f3",
+    "url": "/static/js/2.ab8a4fd2.chunk.js"
+  },
+  {
+    "revision": "9ce0a34b6e7c072e8ae95df9bcbfbe1b",
+    "url": "/static/js/2.ab8a4fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197e6aad27bf5f5ab16d",
+    "url": "/static/js/main.3c0dff7b.chunk.js"
+  },
+  {
+    "revision": "40a33e38825ccb7bc256",
+    "url": "/static/js/runtime-main.7cc58005.js"
+  }
+]);
